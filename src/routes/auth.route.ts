bc144cfcd39a4773b8bce9dc4358f6ccc4b68907
Mirza Shahbaz Ali Baig <mirzashahbazbaig724@gmail.com>
2026-01-16 22:47:36 +0500

    auth and class routes are added

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,91 @@
 import { Router } from "express";
-import { signupSchema } from "../schemas/index.js";
+import { signupSchema, loginSchema } from "../schemas/index.js";
+import { User } from "../models/user.models.js";
+import { errorResponse, successResponse } from "../utils/ApiResponse.js";
+import bcrypt from "bcrypt";
+import { ZodError } from "zod";
+import jwt from "jsonwebtoken";
+import type { AuthRequest } from "../types/index.js";
+import { authenticate } from "../middleware/auth.js";
 
 const router = Router();
 
-
-router.post('/signup' , async (req , res) => {
+router.post("/signup", async (req, res) => {
+  try {
     const validated = signupSchema.parse(req.body);
-    
-})
-router.post('/login')
+    const alreadyUser = await User.findOne({ email: validated.email });
+    if (alreadyUser) {
+      return errorResponse(res, "Email already exists", 400);
+    }
+    const hashedPassword = await bcrypt.hash(validated.password, 10);
+    const newUser = await User.create({
+      name: validated.name,
+      email: validated.email,
+      password: hashedPassword,
+      role: validated.role,
+    });
+    return successResponse(res, 201, newUser);
+  } catch (error) {
+    console.log("ERROR : ", error);
+    if (error instanceof ZodError) {
+      return errorResponse(res, "Zod schema error occured", 400);
+    }
+    return errorResponse(res, "Error Occured while SigningUp", 501);
+  }
+});
+router.post("/login", async (req, res) => {
+  try {
+    const validated = loginSchema.parse(req.body);
+    const user = await User.findOne({ email: validated.email });
+    if (!user) {
+      return errorResponse(res, "Invalid email or password", 400);
+    }
+    const isPassValid = await bcrypt.compare(
+      validated.password,
+      user.password!
+    );
+    if (!isPassValid) {
+      return errorResponse(res, "Invalid email or password", 400);
+    }
+    const token = jwt.sign(
+      {
+        userId: user._id,
+        role: user.role,
+      },
+      process.env.JWT_TOKEN!,
+      {
+        expiresIn: "1h",
+      }
+    );
+    return successResponse(res, 200, token);
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return errorResponse(
+        res,
+        "Zod Schema Error. ALl feilds are required",
+        400
+      );
+    }
+  }
+});
+router.get("/me", authenticate, async (req: AuthRequest, res) => {
+  try {
+    const user = req.user;
+    if (!user) {
+      return errorResponse(res, "User not found in req", 404);
+    }
+    const fetchedUser = await User.findById(user.userId);
+    if (!fetchedUser) {
+      return errorResponse(res, "Invalid Credentials", 401);
+    }
+    const data = {
+      _id: fetchedUser._id,
+      name: fetchedUser.name,
+      email: fetchedUser.email,
+      role: fetchedUser.role,
+    };
+    return successResponse(res, 200, data);
+  } catch (error) {
+    return errorResponse(res, "Server Error Occured", 501);
+  }
+});
---
+++ src/routes/class.route.ts
@@ -0,0 +1,85 @@
+import { Router } from "express";
+import { classSchema } from "../schemas/index.js";
+import { ZodError } from "zod";
+import { errorResponse, successResponse } from "../utils/ApiResponse.js";
+import { authenticate, teacherOnly } from "../middleware/auth.js";
+import { Class } from "../models/class.model.js";
+import type { AuthRequest } from "../types/index.js";
+import { addStudentSchema } from "../schemas/index.js";
+import mongoose from "mongoose";
+const router = Router();
+
+router.post("/", authenticate, teacherOnly, async (req: AuthRequest, res) => {
+  try {
+    const validated = classSchema.parse(req.body);
+    const user = req.user!;
+    const newClass = await Class.create({
+      className: validated.className,
+      teacherId: user.userId,
+    });
+    return successResponse(res, 201, newClass);
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return errorResponse(res, "Invalid Zod Schema", 400);
+    }
+    return errorResponse(res, "Server Error", 500);
+  }
+});
+
+router.post(
+  "/:id/add-student",
+  authenticate,
+  teacherOnly,
+  async (req: AuthRequest, res) => {
+    try {
+      const validated = addStudentSchema.parse(req.body);
+      const user = req.user!;
+      const classId = req.params.id;
+      const fetchedClass = await Class.findById(classId);
+      if (!fetchedClass?.teacherId?.equals(user._id)) {
+        return errorResponse(res, "Teacher is not the owner of the Class", 401);
+      }
+      const studentId = validated.studentId;
+      if (
+        fetchedClass.studentIds.includes(new mongoose.Types.ObjectId(studentId))
+      ) {
+        return errorResponse(res, "Student already added", 400);
+      }
+      fetchedClass?.studentIds.push(new mongoose.Types.ObjectId(studentId));
+      await fetchedClass.save();
+      return successResponse(res, 200, fetchedClass);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return errorResponse(res, "Invalid Zod Schema", 400);
+      }
+      return errorResponse(res, "Server Error", 500);
+    }
+  }
+);
+
+router.get("/:id", authenticate, async (req: AuthRequest, res) => {
+  try {
+    // Get the info of all the class
+    const classId = req.params.id;
+    const user = req.user;
+    const fetchedClass = await Class.findById(classId).populate('teacherId').populate('studentIds');
+    if (!fetchedClass) {
+      return errorResponse(res, "Wrong Class ID", 404);
+    }
+    if (
+      user?.role == "teacher" &&
+      !fetchedClass.teacherId?.equals(new mongoose.Types.ObjectId(user.userId))
+    ) {
+      return errorResponse(res, "Not Authenticated Teacher", 401);
+    }
+    if (
+      user?.role == "student" &&
+      !fetchedClass.studentIds.includes(new mongoose.Types.ObjectId(user.userId))
+    ) {
+        return errorResponse(res , "Not Authenticated Student" , 401)
+    }
+    return successResponse(res , 200 , fetchedClass);
+  } catch (error) {
+    return errorResponse(res , "Sever Error" , 500)
+  }
+});
