cf2e2cbd5135c0ccfeae2d714765a2f4df64a87f
Mirza Shahbaz Ali Baig <mirzashahbazbaig724@gmail.com>
2026-01-19 17:37:04 +0500

    all http server actions completed ws server actions gonna WIP

--- src/routes/attendance.route.ts
+++ src/routes/attendance.route.ts
@@ -1,9 +1,41 @@
 import mongoose from "mongoose";
 import { Router } from "express";
 import { authenticate, teacherOnly } from "../middleware/auth.js";
-const router = Router()
 
+import { attendanceSchema } from "../schemas/index.js";
+import { ZodError } from "zod";
+import { errorResponse } from "../utils/ApiResponse.js";
+import { Class } from "../models/class.model.js";
+import type { AuthRequest } from "../types/index.js";
+import { getActiveResourcesInfo } from "node:process";
+const router = Router();
 
-router.post('/start' ,authenticate , teacherOnly,  async (req , res) => {
-    
-})
+let activeSession: {
+  classId: string;
+  startedAt: Date;
+  attendance: Record<string, string>;
+} | null = null;
+
+router.post(
+  "/start",
+  authenticate,
+  teacherOnly,
+  async (req: AuthRequest, res) => {
+    try {
+      const validated = attendanceSchema.parse(req.body);
+      const classId = validated.classId;
+      const classDb = await Class.findById(classId);
+      if (
+        !classDb ||
+        classDb.teacherId?.toString() !== req.user?.userId.toString()
+      ) {
+        return errorResponse(res, "Forbidden, not class teacher", 403);
+      }
+      active
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return errorResponse(res, "Invalid request schema", 400);
+      }
+    }
+  },
+);
--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,106 @@
-import express from "express"
+import express from "express";
 import { connectDB } from "./config/db.js";
-import authRouter from "./routes/auth.route.js"
-import { authenticate , teacherOnly } from "./middleware/auth.js";
+import authRouter from "./routes/auth.route.js";
+import { authenticate, teacherOnly } from "./middleware/auth.js";
 import { User } from "./models/user.models.js";
 import { errorResponse, successResponse } from "./utils/ApiResponse.js";
 import type { AuthRequest } from "./types/index.js";
-const app = express();
+import { attendanceSchema } from "./schemas/index.js";
+import { Class } from "./models/class.model.js";
+import expressWs from "express-ws";
+import jwt, { type JwtPayload } from "jsonwebtoken"
+import { ZodError } from "zod";
+import dotenv from "dotenv"
 
-app.use(express.json({limit : "16kb"}))
-app.use(express.urlencoded({extended : true , limit : "16kb"}))
-app.use((req, _ , next) => {
-    console.log(`📨 ${req.method} ${req.path}`);
-    next();
+dotenv.config();
+const app = express();
+expressWs(app);
+
+let activeSession: {
+  classId: string;
+  startedAt: Date;
+  attendance: Record<string, string>;
+} | null = null;
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use((req, _, next) => {
+  console.log(`📨 ${req.method} ${req.path}`);
+  next();
 });
 
 app.get("/", (req, res) => {
-    console.log("✅ Root route hit!");
-    return res.status(200).json({ "message": "Server is running correctly" });
+  console.log("✅ Root route hit!");
+  return res.status(200).json({ message: "Server is running correctly" });
 });
-app.use('/auth' , authRouter);
-app.get("/students" , authenticate , teacherOnly ,  async (req : AuthRequest , res) => {
+app.use("/auth", authRouter);
+app.get(
+  "/students",
+  authenticate,
+  teacherOnly,
+  async (req: AuthRequest, res) => {
     console.log("🎯 /students route HIT!");
     console.log("User from token:", req.user);
     try {
-        const students = await User.find({role : 'student'});
-        return successResponse(res , 200 , students)
+      const students = await User.find({ role: "student" });
+      return successResponse(res, 200, students);
     } catch (error) {
-        return errorResponse(res , "Server Error" , 500);
+      return errorResponse(res, "Server Error", 500);
+    }
+  },
+);
+
+app.post(
+  "/attendance/start",
+  authenticate,
+  teacherOnly,
+  async (req: AuthRequest, res) => {
+    try {
+      const validated = attendanceSchema.parse(req.body);
+      const classId = validated.classId;
+      const classDb = await Class.findById(classId);
+      if (
+        !classDb ||
+        classDb.teacherId?.toString() !== req.user?.userId.toString()
+      ) {
+        return errorResponse(res, "Forbidden, not class teacher", 403);
+      }
+      activeSession = {
+        classId: classDb.id.toString(),
+        startedAt: new Date(),
+        attendance: {},
+      };
+      return successResponse(res, 200, {
+        classId: activeSession.classId,
+        startedAt: activeSession.startedAt,
+      });
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return errorResponse(res, "Invalid request schema", 400);
+      }
+    }
+  },
+);
+
+let allWS : any[] = [];
+
+app.ws('/ws' , async (ws , req) => {
+    try {
+        const token = req.query.token;
+        const {userId , role} = jwt.verify(token , process.env.JWT_TOKEN!) as JwtPayload;
+        console.log("DONE" , role);
+    } catch (error) {
+        console.log("ERROR OCCURed")
     }
 })
 
 connectDB()
-.then(() => {
-    app.listen(process.env.PORT! , () => console.log("Server is listening at PORT 8000"))
-})
-.catch((err) => {
+  .then(() => {
+    app.listen(process.env.PORT!, () =>
+      console.log("Server is listening at PORT 8000"),
+    );
+  })
+  .catch((err) => {
     console.log("Database is not Connected ..... ERROR");
-    console.log(err)
-})
+    console.log(err);
+  });
